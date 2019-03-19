6501dbfd3c49156ed72e4e01742fc0e337c457bf
ManhNguyen98 <nguyenthietmanh98@gmail.com>
2019-03-20 00:23:59 +0700

    sửa side bar

--- js/login.js
+++ js/login.js
@@ -24,13 +24,43 @@ window.onscroll = function () {
     stickyHeader();
 }
 
-var headerPage = document.getElementById('headerPage');
-var sticky = headerPage.offsetTop;
-function stickyHeader() {
-    if (window.pageYOffset > sticky) {
-        headerPage.classList.add('sticky');
-    }
-    else {
-        headerPage.classList.remove('sticky');
+let headerPage = document.getElementById('headerPage');
+if (headerPage) {
+    let sticky = headerPage.offsetTop;
+    function stickyHeader() {
+        if (window.pageYOffset > sticky) {
+            headerPage.classList.add('sticky');
+        }
+        else {
+            headerPage.classList.remove('sticky');
+        }
     }
 }
+/*
+***  Open side bar  ***
+***********************
+*/
+$('.js-open-sidebar-1').click(function () {
+    $('#sidebar--large-1').addClass('open');
+    $('#sidebar--small-1').addClass('close');
+    $('#sidebar--large-1').removeClass('close');
+    $('#sidebar--small-1').removeClass('open');
+});
+$('.js-close--sidebar-1').click(function () {
+    $('#sidebar--large-1').removeClass('open');
+    $('#sidebar--small-1').removeClass('close');
+    $('#sidebar--large-1').addClass('close');
+    $('#sidebar--small-1').addClass('open');
+});
+$('.js-open-sidebar-2').click(function () {
+    $('#sidebar--large-2').addClass('open');
+    $('#sidebar--small-2').addClass('close-1');
+    $('#sidebar--large-2').removeClass('close-1');
+    $('#sidebar--small-2').removeClass('open');
+});
+$('.js-close--sidebar-2').click(function () {
+    $('#sidebar--large-2').removeClass('open');
+    $('#sidebar--small-2').removeClass('close-1');
+    $('#sidebar--large-2').addClass('close-1');
+    $('#sidebar--small-2').addClass('open');
+});
